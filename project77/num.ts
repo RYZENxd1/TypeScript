20b265fd87d035a5d1c89bc7c66b53eb6e385bed
RYZENxd1 <160355425+RYZENxd1@users.noreply.github.com>
2024-02-27 08:41:44 -0800

    Add files via upload

---
+++ project77/num.ts
@@ -0,0 +1,22 @@
+// let num1 = 7;
+// let num2 = 8;
+// let sum = num1 + num2;
+// console.log(sum);
+
+// let num1 = 21;
+// let num2 = 4;
+// /
+// console.log(num1 % num2);
+
+// let firstName = "Atia";
+// let lastName = "Khan";
+// console.log(firstName +" " + lastName)
+
+// let num1 = 10
+// let num2 = 20
+// console.log(num1 + num2);
+
+let num1 = 79
+let num2 = 17
+console.log(num1 * num2);
+ 
